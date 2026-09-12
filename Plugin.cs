7e73fad0a603183cb6da2f45acd5fd19e9237b66
Gabriel Medeiros <gabrielmedeirosinacio@gmail.com>
2026-09-11 23:24:05 -0300

    chore: bump to 0.1.1 and declare BepInExPack as an explicit dependency

--- Plugin.cs
+++ Plugin.cs
@@ -13,7 +13,7 @@ namespace SmartCraftStorage
     {
         public const string PluginGuid = "com.zellds.smartcraftstorage";
         public const string PluginName = "SmartCraft-Storage";
-        public const string PluginVersion = "0.1.0";
+        public const string PluginVersion = "0.1.1";
 
         // Jotunn's InputManager mutates ButtonConfig.Name to "<name>!<modGuid>" when it
         // registers the button into ZInput, and it only ever wires the Shortcut's MainKey
