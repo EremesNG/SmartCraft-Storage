6d31b41260de3c11eac7efa507c40fb074135fc8
Gabriel Medeiros <gabrielmedeirosinacio@gmail.com>
2026-09-15 23:30:51 -0300

    chore: bump to 0.5.0

--- Plugin.cs
+++ Plugin.cs
@@ -15,7 +15,7 @@ namespace SmartCraftStorage
     {
         public const string PluginGuid = "com.zellds.smartcraftstorage";
         public const string PluginName = "SmartCraft-Storage";
-        public const string PluginVersion = "0.4.2";
+        public const string PluginVersion = "0.5.0";
 
         internal static Harmony HarmonyInstance;
 
